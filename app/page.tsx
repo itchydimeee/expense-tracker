ea0f91a4b2f2d092248e096ba210f4a5ed3f705d
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-04-30 09:30:28 +0800

    dddd

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { user, error, isLoading } = useUser();
   const router = useRouter();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading..</div>;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
