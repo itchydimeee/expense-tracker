b84b1264650e26feb00415c3e0ec92d2993d4829
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-05-02 14:15:19 +0800

    fix: UI changes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,13 +2,13 @@ import Navbar from '../../components/navbar';
 
 function HomePage() {
   return (
-    <div>
-      <Navbar />
+    <>
       <main className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+        <Navbar />
         <h1 className="text-3xl font-bold">Welcome to the Home Page!</h1>
         <p>This is a blank page. You can add your content here.</p>
       </main>
-    </div>
+    </>
   );
 }
 
