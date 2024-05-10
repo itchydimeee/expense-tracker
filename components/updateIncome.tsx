158ff4da89efb166f2ee5fc43c3351d882857b21
Nobodyknowsss <124364728+Nobodyknowsss@users.noreply.github.com>
2024-05-11 07:29:31 +0800

    implement delete income

--- components/updateIncome.tsx
+++ components/updateIncome.tsx
@@ -1,11 +1,18 @@
-import { useState, FormEvent } from 'react'
-import axios from 'axios'
-import IncomeCategory from './incomeCategories'
+import { useState, FormEvent } from "react";
+import axios from "axios";
+import IncomeCategory from "./incomeCategories";
 
-import { Incomes } from '@/lib/types'
-import DailyLedger from './dailyLedger'
+import { Incomes } from "@/lib/types";
+import DailyLedger from "./dailyLedger";
+import DeleteIncome from "./deleteIncome";
 
-function UpdateIncome({ income, cancelEdit }: { income: Incomes; cancelEdit: () => void  }) {
+function UpdateIncome({
+  income,
+  cancelEdit,
+}: {
+  income: Incomes;
+  cancelEdit: () => void;
+}) {
   const [updatedIncome, setUpdatedIncome] = useState<Incomes>({
     id: income.id,
     categoryId: income.categoryId,
@@ -16,72 +23,83 @@ function UpdateIncome({ income, cancelEdit }: { income: Incomes; cancelEdit: () 
     date: income.date,
     category: income.category,
     dailySummaries: income.dailySummaries,
-    type: 'income'
-  })
- 
+    type: "income",
+  });
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUpdatedIncome({ ...updatedIncome, [event.target.name]: event.target.value })
-  }
+    setUpdatedIncome({
+      ...updatedIncome,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setUpdatedIncome({ ...updatedIncome, categoryId: event.target.value })
-  }
+  const handleCategoryChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setUpdatedIncome({ ...updatedIncome, categoryId: event.target.value });
+  };
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
-    event.preventDefault()
+  const handleSubmit = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
 
     try {
-      const response = await axios.put('/api/updateIncomes', updatedIncome)
-      console.log(response)
+      const response = await axios.put("/api/updateIncomes", updatedIncome);
+      console.log(response);
 
       if (response.data.error) {
-        console.error('Failed to update income:', response.data.error)
+        console.error("Failed to update income:", response.data.error);
       } else {
         window.location.reload();
-        DailyLedger()
+        DailyLedger();
       }
     } catch (error) {
-      console.error('Error updating income:', error)
+      console.error("Error updating income:", error);
     }
-  }
+  };
 
   return (
-    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-700 bg-opacity-50'>
-      <div className='bg-gray-700 rounded-2xl text-white p-4 rounded shadow-md w-full md:w-1/2 xl:w-1/3'>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-700 bg-opacity-50">
+      <div className="bg-gray-700 rounded-2xl text-white p-4  shadow-md w-full md:w-1/2 xl:w-1/3">
         <form onSubmit={handleSubmit}>
-          <label className='block mb-2'>
+          <label className="block mb-2">
             Category:
-            <IncomeCategory onChange={handleCategoryChange} value={updatedIncome.categoryId} />
+            <IncomeCategory
+              onChange={handleCategoryChange}
+              value={updatedIncome.categoryId}
+            />
           </label>
-          <label className='block mb-2'>
+          <label className="block mb-2">
             Description:
             <input
-              type='text'
-              name='description'
+              type="text"
+              name="description"
               value={updatedIncome.description}
               onChange={handleInputChange}
-              className='w-full p-2 pl-10 text-sm bg-gray-300 rounded-xl text-gray-700'
+              className="w-full p-2 pl-10 text-sm bg-gray-300 rounded-xl text-gray-700"
             />
           </label>
-          <label className='block mb-2'>
+          <label className="block mb-2">
             Amount:
             <input
-              type='number'
-              name='amount'
+              type="number"
+              name="amount"
               value={updatedIncome.amount}
               onChange={handleInputChange}
-              className='w-full p-2 pl-10 text-sm rounded-xl bg-gray-300 text-gray-700'
+              className="w-full p-2 pl-10 text-sm rounded-xl bg-gray-300 text-gray-700"
             />
           </label>
-          <div className='flex justify-between'>
+          <div className="flex justify-between">
             <button
-              type='submit'
-              className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded'
+              type="submit"
+              className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
             >
               Update Income
             </button>
+            <DeleteIncome incomeId={income.id || ""} onDelete={cancelEdit} />
             <button
-              className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
               onClick={cancelEdit}
             >
               Cancel
@@ -90,8 +108,7 @@ function UpdateIncome({ income, cancelEdit }: { income: Incomes; cancelEdit: () 
         </form>
       </div>
     </div>
-  )
-
+  );
 }
 
-export default UpdateIncome
+export default UpdateIncome;
