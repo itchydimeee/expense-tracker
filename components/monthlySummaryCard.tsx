cbea6280cc30c21723c97e082ae3a750ee8d2aa5
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-05-20 00:50:27 +0800

    feat: implement monthly summary

--- components/monthlySummaryCard.tsx
+++ components/monthlySummaryCard.tsx
@@ -1,37 +1,79 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { monthsArray } from '../lib/types';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
-
-function MonthlySummaryCard() {
-  return (
-    <Card className='bg-secondary border-none rounded-3xl drop-shadow-lg text-white mb-8'>
-      <CardHeader className='flex flex-row justify-center py-1'>
-        <CardTitle className='font-semibold text-xl'>
-          <button>
-            <FontAwesomeIcon icon={faAngleLeft} />{' '}
-          </button>{' '}
-          May 2024{' '}
-          <button>
-            <FontAwesomeIcon icon={faAngleRight} />
-          </button>
-        </CardTitle>
-      </CardHeader>
-      <div className='border-b border-[#4D4D4D] mx-2'></div>
-      <CardContent className='flex flex-col pt-1 pb-3 px-4'>
-        <div className='flex flex-row justify-between text-sm font-medium text-center'>
-          <div className='w-1/3'>Income</div>
-          <div className='w-1/3'>Expense</div>
-          <div className='w-1/3'>Total</div>
-        </div>
-        <div className='flex flex-row justify-between text-sm font-semibold text-center'>
-          <div className='w-1/3 text-[#578CF7]'>5000.00</div>
-          <div className='w-1/3 text-[#991B1B]'>2000.00</div>
-          <div className='w-1/3 text-[#98FB98]'>3000.00</div>
-        </div>
-      </CardContent>
-    </Card>
-  )
+interface MonthlySummaryCardProps {
+  currentMonth: number;
+  currentYear: number;
+  monthlyIncomeTotals: { [date: string]: number };
+  monthlyExpenseTotals: { [date: string]: number };
+  monthlyProfitTotals: { [date: string]: number };
+  setCurrentMonth: (month: number) => void;
+  setCurrentYear: (year: number) => void;
 }
 
-export default MonthlySummaryCard
+const MonthlySummaryCard = ({
+  currentMonth,
+  currentYear,
+  monthlyIncomeTotals,
+  monthlyExpenseTotals,
+  monthlyProfitTotals,
+  setCurrentMonth,
+  setCurrentYear,
+}: MonthlySummaryCardProps) => {
+  const monthName = monthsArray[currentMonth]
+  const handlePreviousMonth = () => {
+    if (currentMonth === 0) {
+      setCurrentMonth(11);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (currentMonth === 11) {
+      setCurrentMonth(0);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+  };
+
+  return (
+    <div className="max-w-md p-2 rounded-3xl bg-secondary">
+      <div className="text-lg text-center text-white border-b border-gray-900 mb-2 font-bold">
+        <button
+          className="mr-4 text-white text-bold"
+          onClick={handlePreviousMonth}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </button>
+        {monthsArray[currentMonth]} {currentYear}
+        <button
+          className="ml-4 text-white text-bold"
+          onClick={handleNextMonth}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </button>
+      </div>
+      <div className="flex justify-between mb-2">
+        <span className="text-sm text-blue-500">
+          <h1 className='text-white text-sm'> Income</h1>
+          {(monthlyIncomeTotals[monthName] || 0).toFixed(2)}
+        </span>
+        <span className="text-sm text-red-500">
+        <h1 className='text-white text-sm'> Expense</h1>
+          {(monthlyExpenseTotals[monthName] || 0).toFixed(2)}
+        </span>
+        <span className="text-sm text-green-500">
+        <h1 className='text-white text-sm'> Profit</h1>
+          {(monthlyProfitTotals[monthName] || 0).toFixed(2)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default MonthlySummaryCard;
