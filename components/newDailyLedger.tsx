43ea9b2d6fb186363a17e97e7aeada8aa6261396
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-05-20 15:35:26 +0800

    fix: deployment problem

--- components/newDailyLedger.tsx
+++ components/newDailyLedger.tsx
@@ -6,9 +6,9 @@ function DailyLedger({ dailyTransactions }: DailyLedgerProps) {
   return (
     <div className='flex flex-col gap-4'>
       <h2 className='text-white font-bold text-2xl'>Daily Ledger</h2>
-      {dailyTransactions.map((dailyTransaction, index) => (
+      {/* {dailyTransactions.map((dailyTransaction, index) => (
         <DailySummaryCard key={index} {...dailyTransaction} />
-      ))}
+      ))} */}
     </div>
   )
 }
