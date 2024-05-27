927d308d435feb84b950df415e27c27cc2290ac7
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-05-27 20:26:08 +0800

    integration testing: created fetchUser api route test

--- app/api/fetchUser/route.ts
+++ app/api/fetchUser/route.ts
@@ -1,23 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-import { URL } from 'url';
-
-const prisma = new PrismaClient();
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
 
 export async function GET(req: NextRequest) {
   try {
-    const url = new URL(req.url);
-    const email = url.searchParams.get('email');
+    const email = req.nextUrl.searchParams.get('email') ?? ''
+
     if (!email) {
-      return NextResponse.json({ error: 'Email is required' });
+      return NextResponse.json({ error: 'Email is required' }, { status: 400 })
     }
-    const user = await prisma.users.findUnique({ where: { email } });
+    const user = await prisma.users.findUnique({ where: { email: email } })
     if (!user) {
-      return NextResponse.json({ error: 'User not found' });
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
     }
-    return NextResponse.json({ id: user.id });
+    return NextResponse.json({ id: user.id }, { status: 200 })
   } catch (err) {
-    console.log('error', err);
-    return NextResponse.json({ error: 'Failed to fetch user' });
+    console.log('error', err)
+    return NextResponse.json({ error: 'Failed to fetch user' }, { status: 500 })
   }
 }
