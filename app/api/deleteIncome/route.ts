db422b9d93d3e0bae0e939ea1db9ed8f819bcb7c
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-05-27 18:26:24 +0800

    integration testing: created deleteIncome api route test

--- app/api/deleteIncome/route.ts
+++ app/api/deleteIncome/route.ts
@@ -1,31 +1,32 @@
-import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
 
 export async function DELETE(req: NextRequest) {
   try {
-    const body = await req.json();
-    const { id } = body;
-    if (!id) {
+    const incomeId = req.nextUrl.searchParams.get('incomeId') ?? ''
+
+    if (!incomeId) {
       return NextResponse.json(
-        { error: "Expense ID is required" },
+        { error: 'Income ID is required' },
         { status: 400 }
-      );
+      )
     }
 
     await prisma.income.delete({
       where: {
-        id: id,
+        id: incomeId,
       },
-    });
+    })
 
-    return NextResponse.json({ message: "Expense deleted successfully" });
-  } catch (err) {
-    console.error("Error deleting expense:", err);
     return NextResponse.json(
-      { error: "Failed to delete expense" },
+      { message: 'Income deleted successfully' },
+      { status: 200 }
+    )
+  } catch (err) {
+    console.error('Error deleting income:', err)
+    return NextResponse.json(
+      { error: 'Failed to delete income' },
       { status: 500 }
-    );
+    )
   }
 }
--- components/deleteIncome.tsx
+++ components/deleteIncome.tsx
@@ -1,32 +1,34 @@
-import React from "react";
-import axios from "axios";
+import React from 'react'
+import axios from 'axios'
 
 interface Props {
-  incomeId: string;
-  onDelete: () => void;
+  incomeId: string
+  onDelete: () => void
 }
 
 const DeleteIncome: React.FC<Props> = ({ incomeId, onDelete }) => {
   const handleDelete = async () => {
     try {
-      await axios.delete("/api/deleteIncome", { data: { id: incomeId } });
-      onDelete();
-      window.location.reload();
+      await axios.delete('/api/deleteIncome', {
+        params: { incomeId: incomeId },
+      })
+      onDelete()
+      window.location.reload()
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
     <button
-      id="delete-button"
-      data-testid="delete-income"
+      id='delete-button'
+      data-testid='delete-income'
       onClick={handleDelete}
-      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2 mr-2"
+      className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2 mr-2'
     >
       Delete
     </button>
-  );
-};
+  )
+}
 
-export default DeleteIncome;
+export default DeleteIncome
