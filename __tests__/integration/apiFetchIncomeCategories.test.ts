e7809f8df862839fa1dd93ff967c7154ce0c63a7
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-05-27 19:03:30 +0800

    integration testing: created fetchIncomeCategories api route test

--- __tests__/integration/apiFetchIncomeCategories.test.ts
+++ __tests__/integration/apiFetchIncomeCategories.test.ts
@@ -1,10 +1,10 @@
 /**
  * @jest-environment node
  */
-import { GET } from '@/app/api/fetchExpenseCategories/route'
+import { GET } from '@/app/api/fetchIncomeCategories/route'
 import prisma from '@/lib/prisma'
 
-describe('GET from /api/fetchExpenseCategories', () => {
+describe('GET from /api/fetchIncomeCategories', () => {
   it('should return data with status 200', async () => {
     const response = await GET()
 
@@ -12,7 +12,7 @@ describe('GET from /api/fetchExpenseCategories', () => {
   })
 
   it('should return error with status 500 when database error occurs', async () => {
-    jest.spyOn(prisma.expenseCategories, 'findMany').mockImplementation(() => {
+    jest.spyOn(prisma.incomeCategories, 'findMany').mockImplementation(() => {
       throw new Error('Database error')
     })
 
