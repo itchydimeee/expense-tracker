fef6194fc8a1431a06bbf7e47d23e9af99c0f6d7
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-05-27 22:16:54 +0800

    integration testing: created users api route test files

--- __tests__/integration/apiUsers.test.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @jest-environment node
- */
-import { GET, POST } from '@/app/api/users/route'
-import prisma from '@/lib/prisma'
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -18,12 +18,12 @@ export async function GET(req: NextRequest) {
       include: { expenses: true, income: true },
     })
     if (!user) {
-      return NextResponse.json({ error: 'User not Found' })
+      return NextResponse.json({ error: 'User not Found' }, { status: 404 })
     }
     return NextResponse.json({ id: user.id }, { status: 200 })
   } catch (err) {
     console.log('error', err)
-    return NextResponse.json({ error: 'Failed to fetch user' })
+    return NextResponse.json({ error: 'Failed to fetch user' }, { status: 500 })
   }
 }
 
@@ -31,17 +31,23 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
     if (await prisma.users.findUnique({ where: { email: body.email } })) {
-      return NextResponse.json({
-        error: 'User with this email already exists',
-      })
+      return NextResponse.json(
+        {
+          error: 'User with this email already exists',
+        },
+        { status: 409 }
+      )
     }
     const { auth0Id, email, username } = body
     const newUser = await prisma.users.create({
       data: { auth0Id, email, username },
     })
-    return NextResponse.json(newUser)
+    return NextResponse.json(newUser, { status: 200 })
   } catch (err) {
     console.log('error', err)
-    return NextResponse.json({ error: 'Failed to create user' })
+    return NextResponse.json(
+      { error: 'Failed to create user' },
+      { status: 500 }
+    )
   }
 }
